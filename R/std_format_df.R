227da047dd4a8432f5bd2a28a7203a8d4776e3c2
Martin <martinbbanchero@gmail.com>
2020-03-10 10:29:37 +0100

    DESCRIPTION file

--- R/std_format_df.R
+++ R/std_format_df.R
@@ -1,14 +1,14 @@
 std_format_df <- function(df){    # Format the df
   well_ids <- 3:ncol(df)
   df[, well_ids] <- as.numeric(sub(",", ".", as.character(unlist(df[, well_ids]))))
-  
+
   # Apply dplyr
-  df <- gather(df, 
-               key = 'Wells', 
+  df <- dplyr::gather(df,
+               key = 'Wells',
                value = 'Measurement', c(-1,-2))
-  
+
   df$Wells <- gsub('(^[A-Z])([0-9]$)', '\\10\\2', df$Wells)
-  
-  df_measurements <- select(df, Wells, everything()) #Swap the well column to the first column 
+
+  df_measurements <- dplyr::select(df, Wells, everything()) #Swap the well column to the first column
   df_measurements
 }
