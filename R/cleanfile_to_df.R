24f3dfcf00c6a6f7bf5d77d342fd156f3b75467d
Martin <martinbbanchero@gmail.com>
2020-03-10 16:00:58 +0100

    cleanfile_to_df

--- R/cleanfile_to_df.R
+++ R/cleanfile_to_df.R
@@ -3,7 +3,7 @@
 #' \code{cleanfile_to_df} takes a clean file which is a string type and using
 #' read.table generates a dataframe.
 #' @param cleanfile A string
-#' @return Dataframe
+#' @return tidy dataframe
 #' @example
 #' cleanfile_to_df(cleanfile)
 #' @seealso \code{\link[utils]{read.table}}
