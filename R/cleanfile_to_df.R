fbfa0413f57932e68eb6844b78e5989da8cfcabf
Martin <martinbbanchero@gmail.com>
2020-03-10 14:42:13 +0100

    Vignettes

--- R/cleanfile_to_df.R
+++ R/cleanfile_to_df.R
@@ -1,9 +1,19 @@
+#'Make data frame using a clean file.
+#'
+#' \code{cleanfile_to_df} takes a clean file which is a string type and using
+#' read.table generates a dataframe.
+#' @param cleanfile A string
+#' @return Dataframe
+#' @example
+#' cleanfile_to_df(cleanfile)
+#' @seealso \code{\link[utils]{read.table}}
+
 cleanfile_to_df <- function(cleanfile) {
-  df <- read.table(textConnection(cleanfile), 
+  df <- utils::read.table(textConnection(cleanfile),
                    header = TRUE,
                    sep = '\t',
-                   stringsAsFactors = FALSE, 
-                   colClasses = 'character', 
+                   stringsAsFactors = FALSE,
+                   colClasses = 'character',
                    comment.char = "")
   return(df)
 }
