d4a05866543f7777d7aae62f513431e7973e98a7
Martin <martinbbanchero@gmail.com>
2020-03-12 14:33:39 +0100

    utils

--- R/mpxtractor_utils.R
+++ R/mpxtractor_utils.R
@@ -1,7 +1,7 @@
-#' Check that one file is provided.
-#'
-#' @return Error if more than one file is given.
-#' @seealso \code{\link{plater}}
+# Check that one file is provided.
+#
+# @return Error if more than one file is given.
+# @seealso \code{\link{plater}}
 check_that_only_one_file_is_provided <- function(file){
   if (length(file) > 1) {
     stop(paste0("Sorry, only one file should be provided, but you provided ",
@@ -10,12 +10,12 @@ check_that_only_one_file_is_provided <- function(file){
 
 }
 
-#' Check the file path.
-#'
-#' @seealso \code{\link{plater}}
-#' @return Error if the file is null or if the file does not exist. Then check if
-#' the extension is .txt
-#'
+# Check the file path.
+#
+# @seealso \code{\link{plater}}
+# @return Error if the file is null or if the file does not exist. Then check if
+# the extension is .txt
+#
 check_file_path <- function(file){
   if (is.null(file) || !file.exists(file)) {
     stop(paste0("Sorry, can't find your file '", file, "'."),
@@ -27,11 +27,11 @@ check_file_path <- function(file){
   }
 }
 
-#' Check that file is not empty
-#'
-#' @seealso \code{\link{plater}}
-#' @return Error if the file is empty.
-#'
+# Check that file is not empty
+#
+# @seealso \code{\link{plater}}
+# @return Error if the file is empty.
+#
 check_that_file_is_non_empty <- function(file)
 {
   if (length(readLines(file)) == 0) {
