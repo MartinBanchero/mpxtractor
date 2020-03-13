af37c71a99c955788e8481c38515711b93a432e6
Martin <martinbbanchero@gmail.com>
2020-03-13 16:11:00 +0100

    Updata multiple files function

--- R/read_multiple_plates.R
+++ R/read_multiple_plates.R
@@ -1,11 +1,11 @@
 #' Multiple plates files
 #'
 #' @export
-read_multiple_plates <- function(reader_type = NULL, dirFiles = NULL, file_pattern = NULL, filesname = NULL,
-  plate_names = NULL)
-  {
+read_multiple_plates <- function(reader_type = NULL, dirFiles = NULL, file_pattern = NULL,
+                                 filesname = NULL, plate_names = NULL)
+{
   files <- get_input_read_multifiles(folder = dirFiles, pattern = file_pattern,
-    filebyname = filesname)
+                                     filebyname = filesname)
   lapply(files, check_file_path)
   if (is.null(plate_names))
   {
@@ -15,19 +15,19 @@ read_multiple_plates <- function(reader_type = NULL, dirFiles = NULL, file_patte
   {
     stop("files and plate_names must have the same length.")
   }
-  if (is.null(reader_type)) {
+  if (is.null(reader_type))
+  {
     stop("Sorry, one reader type must to be specified.")
   }
-  if (toupper(reader_type) != toupper("spectramax")  &&
-      toupper(reader_type) != toupper("multiscango") &&
-      toupper(reader_type) != toupper("fluorstar") )
+  if (toupper(reader_type) != toupper("spectramax") && toupper(reader_type) !=
+      toupper("multiscango") && toupper(reader_type) != toupper("fluorstar"))
   {
     stop("Sorry, the micro-plate readers must to be spectramax, multiscango or fluorstar.")
   }
   list_of_data_frames <- Map(f = function(file, plate_name)
   {
     tryCatch(expr = {
-      p <- read_spectraMax_data(file)  #One function for each machine
+      p <- type_of_reader(file, reader_type)
       p$Plate <- plate_name
       p
     }, error = function(e)
@@ -42,9 +42,8 @@ read_multiple_plates <- function(reader_type = NULL, dirFiles = NULL, file_patte
   result
 }
 
-# Get input to give to the function that read multiple files
-#
-# @return filenames string
+# Get input to give to the function that read multiple files @return filenames
+# string
 get_input_read_multifiles <- function(folder = NULL, pattern = NULL, filebyname = NULL)
 {
   if (is.null(folder) && !is.null(pattern) && is.null(filebyname))
@@ -58,16 +57,17 @@ get_input_read_multifiles <- function(folder = NULL, pattern = NULL, filebyname 
   if (!is.null(folder))
   {
 
-# List the files present in the given folder and if pattern is given look for files with that pattern
-#
-get_files <- function(folder, pattern = NULL)
+    # List the files present in the given folder and if pattern is given look for
+    # files with that pattern
+    get_files <- function(folder, pattern = NULL)
     {
       if (is.null(pattern))
       {
         lst_files <- list.files(path = folder, recursive = TRUE, full.names = TRUE)
         return(lst_files)
       }
-      lst_files <- list.files(path = folder, pattern = pattern, recursive = TRUE, full.names = TRUE)
+      lst_files <- list.files(path = folder, pattern = pattern, recursive = TRUE,
+                              full.names = TRUE)
       return(lst_files)
     }
 
@@ -81,4 +81,24 @@ get_files <- function(folder, pattern = NULL)
   }
 }
 
+# Receive the name of the machine and call the specific function to read the
+# data.
+type_of_reader <- function(file, reader_type)
+{
+  if (toupper(reader_type) == toupper("spectramax"))
+  {
+    p <- read_spectraMax_data(file)  #One function for each machine
+    return(p)
+  }
+  if (toupper(reader_type) == toupper("multiscango"))
+  {
+    # p <- read_multiscanGO_data(file) #One function for each machine
+    print("multiscanGO")
+  }
+  if (toupper(reader_type) == toupper("fluorStar"))
+  {
+    # p <- read_fluorStar_data(file) #One function for each machine
+    print("fluorStar")
+  }
 
+}
