534cbbb3f63af979b53afb70e30b35c004b23385
Martin <martinbbanchero@gmail.com>
2020-03-13 17:23:44 +0100

    MultiscanGO

---
+++ R/read_multiscanGO_data.R
@@ -0,0 +1,71 @@
+#' Function specific to read output .txt files from multiscanGO readers.
+#'
+#' \code{read_multiscanGO_data} returns the data in the .txt as tibble data frame
+#'
+#' @export
+#' @examples
+#' file_path <- system.file("extdata", "multiscanGO_1streading.txt", package = "mpxtractor")
+#'
+#' # Data is store as a tibble
+#' data <- read_multiscanGO_data(
+#'    file = file_path)
+#'
+#' # Now data is tidy
+#' head(data)
+
+# Main function
+read_read_multiscanGO_data <- function(file)
+{
+  check_that_only_one_file_is_provided(file)
+  check_file_path(file)
+  check_that_file_is_non_empty(file)
+  clean_file <- get_raw_file_clean_multiscanGO(file)
+  df <- format_df(clean_file)
+  df_tmp <- add_header(df)
+  df_final_format <- final_format_df(df_tmp)
+  df_result <- set_well_ids(df_final_format)
+  df_result_tidy <- tidyr::as_tibble(df_result)
+  df_result_tidy
+}
+#get the raw file
+get_raw_file_clean_multiscanGO <- (file){
+  raw_file <- readLines(file, warn = FALSE, encoding = "latin1")
+  raw_file_clean <- raw_file[which(raw_file != "")]#Remove empty space
+  clean_file <- raw_file_clean[-c(1,2)]#Remove firs to lines( I have to ask if we need this info)
+}
+
+# format_df
+format_df <- (clean_file){
+  idx <- grepl('Reading', file)#Get indx of readings
+  df <- read.table(text = file[!idx])#get all the lines whith numbers
+  
+  wd = diff(c(which(idx), length(idx) + 1)) - 1#Calculate the number of lines between readings
+  
+  df <- cbind(Reading = rep(file[idx], wd),df)#Assign reading to corresponding values 
+  num_read <- gsub("\\tReading:", "\\1", df$Reading)# Leave only the number of Readings
+  df$Reading <- as.numeric(num_read)# Add column reading with the number of reading as numeric
+}
+# Header
+add_header <- (df){
+  df2 <- df[-1]
+  colnames(df2) <- LETTERS[1:ncol(df2)]
+  df3 <- cbind(Reading = df[,1], df2)
+}
+
+final_format_df <- (df3){
+  df_tmp <- df3 %>%
+    gather(Col, Measurement, -Reading) %>%
+    group_by(Reading, Col) %>%
+    mutate(rowID = seq_along(Col)) %>% 
+    ungroup() %>%
+    mutate(name = interaction(Col, rowID, sep = ''), Col = NULL, rowID = NULL ) %>%
+    spread(name, Measurement )
+    df_tmp2  <- tidyr::gather(df_tmp, key = 'Wells', value = 'Measurement', -Reading)
+}
+
+set_well_ids <- (df_tmp2){
+  wells <- df_tmp2$Wells
+  wellsname <- gsub('(^[A-Z])([0-9]$)', '\\10\\2', wells)
+  df_tmp2$Wells <- wellsname
+  df_result <- dplyr::select(df_tmp2, Wells, everything())  #Swap the well column to the first column
+}
